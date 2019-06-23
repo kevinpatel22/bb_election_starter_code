504fddfd19a20d659dfef778f643ff38339a6030
Kevin <mail.kevinpatel@gmail.com>
2019-06-23 18:26:01 -0400

    completed part 1 of the election exercise

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const ul = document.querySelector('#candidates');
 
+    axios.get('https://bb-election-api.herokuapp.com/')
+    .then(response => {
+        console.log(response.data);
+        let candidates = response.data.candidates;
+        candidates.forEach(candidate => {
+            let li = document.createElement('li');
+            li.innerText = 'Name: ' + candidate.name + '\n' + 'Votes: ' + candidate.votes + '\n\n\n';
+            ul.appendChild(li);
+        });
+            
+        });
+    
 });
